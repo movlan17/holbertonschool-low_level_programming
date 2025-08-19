e19e3700a9d22f77316126f5d9956261617edbc9
movlan17 <movlanomerov00@gmail.com>
2025-08-19 13:18:04 +0000

    X

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
+ * print_name - Prints a name using a callback function
+ * @name: The name to print
+ * @f: A pointer to a function that takes a string and returns nothing
  *
- * Return: no return.
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name != NULL && f != NULL)
 		f(name);
 }
-
