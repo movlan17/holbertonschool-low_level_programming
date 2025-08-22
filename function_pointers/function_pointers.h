d977a74abb9ea4e462702df6fa808b128b2dea77
movlan17 <movlanomerov00@gmail.com>
2025-08-22 10:54:28 +0000

    sd

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,26 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-void print_name(char *name, void (*f)(char *));
+#include <stddef.h>
+
+/**
+ * array_iterator - bir fonksiyonu her dizi elemanına uygular
+ * @array: tamsayı dizisi
+ * @size: dizinin eleman sayısı
+ * @action: uygulanacak fonksiyon işaretçisi
+ *
+ * Return: hiçbir şey döndürmez
+ */
+void array_iterator(int *array, size_t size,
+		    void (*action)(int));
+
+/**
+ * print_elem - bir tamsayıyı yazdırır
+ * @elem: yazdırılacak tamsayı
+ *
+ * Return: hiçbir şey döndürmez
+ */
+void print_elem(int elem);
 
 #endif /* FUNCTION_POINTERS_H */
 
