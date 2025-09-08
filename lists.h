a5f17df871ba18bfc74285c4ee405ff95643cc78
movlan17 <movlanomerov00@gmaail.com>
2025-09-08 13:07:47 +0000

    Fix: rename lists.h and clean code

--- lists.h
+++ lists.h
@@ -1,13 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
- * struct list_s - singly linked list node
+ * struct list_s - singly linked list
  * @str: string - malloc'ed string
  * @len: length of the string
  * @next: points to the next node
- *
- * Description: singly linked list node structure
  */
 typedef struct list_s
 {
@@ -19,4 +19,3 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
-
