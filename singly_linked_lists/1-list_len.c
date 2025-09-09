c032dab1e74e13ff9e5a1a331ee0f95c40b9a07a
movlan17 <movlanomerov00@gmail.com>
2025-09-09 14:34:35 +0000

    mds

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -7,19 +7,15 @@
  *
  * Return: the number of nodes
  */
-size_t list_len(const list_t *h);
+size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
 
     while (h != NULL)
     {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-
         count++;
-	h = h->next;
+        h = h->next;
     }
-    return (count);
+    return count;
 }
+
