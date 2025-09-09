def890cded09bb0a22193ec247446ba601ff2264
movlan17 <movlanomerov00@gmail.com>
2025-09-09 14:30:13 +0000

    mds

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -11,15 +11,15 @@ size_t list_len(const list_t *h);
 {
 	size_t count = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
 
-		count++;
-		h = h->next;
-	}
-	return (count);
+        count++;
+	h = h->next;
+    }
+    return (count);
 }
