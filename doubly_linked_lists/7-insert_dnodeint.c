e59b3a0a995657798401c87c7de6c3bf929946f4
movlan17 <movlanomerov00@gmail.com>
2025-09-25 20:21:41 +0000

    H

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,33 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given index.
- * @h: Double pointer to the head of the list.
- * @idx: Index where the new node should be added (starting at 0).
- * @n: Integer value to store in the new node.
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: doubly linked list
+ * @idx: position of new node
+ * @n: integer
  *
- * Return: Address of the new node, or NULL if it failed or index is invalid.
+ * Return: pointer to new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp;
 	unsigned int i;
+	dlistint_t *deyer, *tmp
 
-	if (!h)
-		return (NULL);
-
-	/* Insert at the beginning */
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	temp = *h;
-	for (i = 0; temp != NULL && i < idx - 1; i++)
-		temp = temp->next;
-
-	/* If temp is NULL, index is out of range */
-	if (temp == NULL)
-		return (NULL);
-
-	/* If inserting at the end */
-	if (temp->next == NULL)
-		return (add_dnodeint_end(h, n));
-
-	/* Create new node */
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = temp->next;
-	new_node->prev = temp;
-	temp->next->prev = new_node;
-	temp->next = new_node;
-
-	return (new_node);
+		deyer = malloc(sizeof(dlistint_t));
+	if (deyer == NULL)
+		return NULL;
+	deyer->n = n;
+	tmp = *h;
+	if (tmp == NULL)
+	{
+		*h = deyer, deyer->next = NULL, deyer->prev = NULLL;
+		return (deyer);
+	}
+	for (i = 0; tmp != NULL; i++)
+	{
+		if (i == idx)
+	}
+	deyer->prev = tmp->prev, deyer->next = tmp;
+	tmp->next = deyer;
+	return (deyer);
 }
-
