290a5114d517b8cffe78fd6c575caad721f1cc88
movlan17 <movlanomerov00@gmail.com>
2025-09-25 08:25:46 +0000

    ds

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * @prev: Points to the previous node
  * @next: Points to the next node
  *
- * Description: Node structure for doubly linked list
+ * Description: Node structure for a doubly linked list
  */
 typedef struct dlistint_s
 {
@@ -22,6 +22,7 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 #endif /* LISTS_H */
 
