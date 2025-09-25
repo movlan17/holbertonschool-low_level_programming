052d779bf2696f3a9f994c189c1497a7126f6135
movlan17 <movlanomerov00@gmail.com>
2025-09-25 07:41:17 +0000

    dsa

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,8 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-#include <stddef.h>
-
 /**
  * struct dlistint_s - Doubly linked list node
  * @n: Integer stored in the node
@@ -17,14 +14,4 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-
-/**
- * dlistint_len - Returns the number of elements in a linked dlistint_t list
- * @h: Pointer to the head of the list
- *
- * Return: Number of elements in the list
- */
-size_t dlistint_len(const dlistint_t *h);
-
-#endif /* LISTS_H */
 
