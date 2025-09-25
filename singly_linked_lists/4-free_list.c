f24945e5bbabeacd6a87971829c5d280a3d4111d
movlan17 <movlanomerov00@gmail.com>
2025-09-25 20:05:53 +0000

    H

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,10 +10,10 @@ void free_list(list_t *head)
 {
 	list_t *tmp;
 
-	while (head != NULL )
+	while (head != NULL)
 	{
-		tmp = head-> next;
-		free(head-> str);
+		tmp = head->next;
+		free(head->str);
 		free(head);
 		head = tmp;
 	}
