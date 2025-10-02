7d08f60e69832a768a9c7a3c14f69600e11cbc39
movlan17 <movlanomerov00@gmail.com>
2025-10-02 08:52:48 +0000

    gj

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,19 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
+#include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the number to check
+ * _abs - computes the absolute value of an integer
+ * @n: the integer to compute absolute value for
  *
- * Return: 1 if n > 0, 0 if n == 0, -1 if n < 0
+ * Return: the absolute value of n
  */
-int print_sign(int n);
-
-#endif /* MAIN_H */
-
+int _abs(int n)
+{
+    return (n < 0 ? -n : n);
+}
