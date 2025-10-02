0382417cb47f6174be4b91a8f50c20d0eb15dede
movlan17 <movlanomerov00@gmail.com>
2025-10-02 08:27:03 +0000

    ds

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,5 +6,12 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to be checked
+ * Return: 1 if letter, 0 otherwise
+ */
+int _isalpha(int c);
+
 #endif /* MAIN_H */
 
