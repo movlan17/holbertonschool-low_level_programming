d7aa933a37486442fd5c679e78933172ddc384e2
movlan17 <movlanomerov00@gmail.com>
2025-10-02 08:44:18 +0000

    hi

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,13 +5,15 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: character to be checked
- * Return: 1 if letter, 0 otherwise
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if n > 0, 0 if n == 0, -1 if n < 0
  */
-int _isalpha(int c);
+int print_sign(int n);
 
 #endif /* MAIN_H */
 
