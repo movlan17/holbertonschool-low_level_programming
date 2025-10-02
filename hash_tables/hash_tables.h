220842e79e96458b4f262812d54b33965f2bd930
movlan17 <movlanomerov00@gmail.com>
2025-10-02 14:46:11 +0000

    jgkl

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,13 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-/**
- * struct hash_node_s - Node of a hash table
- *
- * @key: The key, a string (unique in the hash table)
- * @value: The value corresponding to a key
- * @next: Pointer to the next node in the list (handles collisions)
- */
+#include <stdlib.h>
+#include <stddef.h>
+
 typedef struct hash_node_s
 {
 	char *key;
@@ -15,18 +11,19 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
-/**
- * struct hash_table_s - Hash table data structure
- *
- * @size: The size of the array
- * @array: An array of size @size. Each element is a pointer to the first
- *         node of a linked list (for collision resolution).
- */
 typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_delete(hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
 
