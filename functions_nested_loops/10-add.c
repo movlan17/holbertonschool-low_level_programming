be2e6fe9fcaa2bfc89db6f74c6d35e4ca7c3fea1
movlan17 <movlanomerov00@gmail.com>
2025-10-02 10:12:23 +0000

    sad

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * add - adds two integers and returns the result
- * @a: first integer
- * @b: second integer
+ * add - Adds two integers and returns the result
+ * @a: First integer
+ * @b: Second integer
  *
- * Return: sum of a and b
+ * Return: The sum of a and b
  */
 int add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_to_98(int n);
+int add(int, int);
 
 #endif /* MAIN_H */
 
