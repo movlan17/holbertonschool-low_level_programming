1f9df57deb21f05ad06b6386771f8771edd620bb
movlan17 <movlanomerov00@gmail.com>
2025-10-02 13:44:54 +0000

    dsa

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,41 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string (unique in the HashTable)
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ *
+ * Description: Node structure for a hash table.
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Structure of a hash table
+ *
+ * @size: Size of the array
+ * @array: Array of pointers to hash_node_t (linked list for chaining)
+ *
+ * Description: Structure representing a hash table with
+ * separate chaining for collision handling.
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+/* Function prototype */
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /* HASH_TABLES_H */
+
