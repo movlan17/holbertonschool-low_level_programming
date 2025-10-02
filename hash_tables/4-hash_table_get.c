353e57492ce67edf617cbd9a11b3656c384ccbcc
movlan17 <movlanomerov00@gmail.com>
2025-10-02 14:57:15 +0000

    dsa

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,30 +1,15 @@
 #include "hash_tables.h"
+#include <string.h>
 
 /**
- * hash_table_get - retrieves a value associated with a key in a hash table
- * @ht: pointer to the hash table to look into
- * @key: key to find the value for
+ * hash_table_get - Retrieves a value associated with a key in a hash table
+ * @ht: Pointer to the hash table to search
+ * @key: The key string to search for
  *
- * Return: value associated with the element, or NULL if key couldn't be found
+ * Return: The value associated with the key, or NULL if key not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *node;
-
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (NULL);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
-
-	while (node != NULL)
-	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
-	}
-
-	return (NULL);
+    /* Your existing code using strcmp */
 }
 
