32253022ddc1a4eb01b6308c670ae29c0b015d21
movlan17 <movlanomerov00@gmail.com>
2025-10-02 09:22:08 +0000

    dsa

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,9 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
-void jack_bauer(void);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
 
 #endif /* MAIN_H */
 
