b4b74ff77c64a77acde7aa6c6d2161c145919397
movlan17 <movlanomerov00@gmail.com>
2025-10-05 11:12:37 +0000

    sdasad

--- 1-strdup.c
+++ 1-strdup.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+<<<<<<< HEAD
 
 /**
  * _strdup - returns a pointer to a newly allocated space in memory,
@@ -7,6 +8,17 @@
  *
  * Return: pointer to the duplicated string,
  * or NULL if str = NULL or malloc fails
+=======
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string given as a parameter.
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string, or NULL if str is NULL
+ *         or if memory allocation fails
+>>>>>>> 456df29 (sdasad)
  */
 char *_strdup(char *str)
 {
@@ -16,6 +28,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
+<<<<<<< HEAD
 	/* find length of the string */
 	while (str[len] != '\0')
 		len++;
@@ -32,6 +45,19 @@ char *_strdup(char *str)
 
 	dup[len] = '\0';
 
+=======
+	for (i = 0; str[i]; i++)
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		dup[i] = str[i];
+
+	dup[i] = '\0';
+>>>>>>> 456df29 (sdasad)
 	return (dup);
 }
 
--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+<<<<<<< HEAD
 
 char *_strdup(char *str);
 #endif
+=======
+char *_strdup(char *str);
+
+#endif
+
+>>>>>>> 456df29 (sdasad)
